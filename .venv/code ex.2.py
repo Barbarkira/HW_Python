00fee727bec8aa23c14342fcfe597c16757452e2
Barbarkira <gusevavarvara18@gmail.com>
2024-10-17 11:39:24 +0300

    added ex.2 code
    Signed-off-by: Barbarkira <gusevavarvara18@gmail.com>

--- .venv/code ex.2.py
+++ .venv/code ex.2.py
@@ -1,7 +1,7 @@
 def PrefixOfWord(sentence: str, searchWord: str) -> int:
     words = sentence.split()
 
-    for index, word in enumerate(words, start=1):
+    for index, word in enumerate(words, start = 1):
 
         if word.startswith(searchWord):
             return index  # Return the 1-based index
