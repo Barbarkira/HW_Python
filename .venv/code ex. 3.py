9a70b0fbb173b829904a45a6ef46c3a8097abc89
Barbarkira <gusevavarvara18@gmail.com>
2024-10-21 14:04:54 +0300

    added ex.3 code
    Signed-off-by: Barbarkira <gusevavarvara18@gmail.com>

--- .venv/code ex. 3.py
+++ .venv/code ex. 3.py
@@ -9,31 +9,41 @@
 # Используйте модуль: https://docs.python.org/3.3/library/random.html
 
 import random
-lengthp = int(input("Задайте длину пароля: "))
-def password(length):
-    if length < lengthp:
-        print('Пароль ненадежен')
-    lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'  # строчные буквы
-    uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'  # заглавные буквы
-    digits = '0123456789'  # цифры
-    symbols = '!@#$%^&*()-_=+[]{}|;:,.<>?/'  # специальные символы
-    fullpassword = [
-        random.choice(lowercase_letters),
-        random.choice(uppercase_letters),
-        random.choice(digits),
+import string
+
+def generate_password(length):
+    if length < 4:  # Минимальная длина пароля
+        print("Пароль должен содержать как минимум 4 символа.")
+        return None
+
+    lowercase_letters = string.ascii_lowercase  # строчные буквы
+    uppercase_letters = string.ascii_uppercase  # заглавные буквы
+    digits = string.digits  # цифры
+    special_characters = string.punctuation
+
+    password_characters = (
+        random.choice(lowercase_letters) +
+        random.choice(uppercase_letters) +
+        random.choice(digits) +
         random.choice(special_characters)
-    ]
-    if length > lengthp:
-        all_characters = lowercase_letters + uppercase_letters + digits + special_characters
-        fullpassword += random.choices(all_characters, k=length - 4)
-    random.shuffle(fullpassword)
-    return ''.join(fullpassword)
+    )
+
+
+    all_characters = lowercase_letters + uppercase_letters + digits + special_characters
+    password_characters += ''.join(random.choice(all_characters) for _ in range(length - 4))
+
+    password = ''.join(random.sample(password_characters, len(password_characters)))
+
+    return password
+
+
 try:
-    password_length = int(input("Введите длину пароля (не менее 4 символов): "))
-    new_password = password(password_length)
-    print("Сгенерированный пароль: ", new_password)
-except ValueError as e:
-    print(e)
+    length = int(input("Введите длину пароля (минимум 4): "))
+    generated_password = generate_password(length)
+    if generated_password:
+        print(f"Сгенерированный пароль: {generated_password}")
+except ValueError:
+    print("Пожалуйста, введите корректное число.")
 
 
 
