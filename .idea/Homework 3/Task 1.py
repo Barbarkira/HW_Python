77cd0995f4356e27d9c6769c87038e18830b7d83
Barbarkira <gusevavarvara18@gmail.com>
2024-11-21 20:37:52 +0300

    added Test
    Signed-off-by: Barbarkira <gusevavarvara18@gmail.com>

--- .idea/Homework 3/Task 1.py
+++ .idea/Homework 3/Task 1.py
@@ -5,18 +5,14 @@ def read_data(path):
     data = open(path, 'rt').read()
     return data.split('\n')
 
-
 def word_dictionary(data) -> dict:
-
     frequency_dic = {}
     for word in data:
         if word not in frequency_dic:
             frequency_dic[word] = 1
-            continue
-        if word in frequency_dic:
+        elif word in frequency_dic:
             frequency_dic[word] += 1
     return frequency_dic
-
 
 def create_chapters_dicts(data):
     deliminators_indicis = [i for i, e in enumerate(data[1:]) if e == '[new chapter]']
@@ -32,59 +28,68 @@ def create_chapters_dicts(data):
     chapter_frequencies.append(chapter_frequency)
     return chapter_frequencies
 
-def read_data(path: str) -> list:
-    with open(path, 'r', encoding='utf-8') as file:
-        return file.readlines()
-
-def create_chapters_dicts(data: list) -> dict:
-    chapters = {}
-    for i, line in enumerate(data):
-        chapters[i] = line.strip()
-    return chapters
-
 def chapter_frequency(path: str, target_word: str) -> float:
     data = read_data(path)
     chapter_frequencies = create_chapters_dicts(data)
-
-    number_of_chapters = len(chapter_frequencies)
-    number_of_chapters_with_target_word = 0
-
-    for chapter in chapter_frequencies.values():
-        if target_word in chapter.split():
-            number_of_chapters_with_target_word += 1
-    if number_of_chapters == 0:
-        return 0.0
-
-    chapter_freq = number_of_chapters_with_target_word / number_of_chapters
+    num_chapters_with_word = 0
+    total_chapters = len(chapter_frequencies)
+    for chapter in chapter_frequencies:
+        if target_word in chapter:
+            num_chapters_with_word += 1
+    chapter_freq = num_chapters_with_word / total_chapters if total_chapters > 0 else 0
     return chapter_freq
-def term_frequency(target_word: str, target_chapter_idx: int, chapters: dict) -> float:
-    chapter_text = chapters.get(target_chapter_idx, "")
-    words = chapter_text.split()
-    if not words:
-        return 0.0
-    count_target_word = words.count(target_word)
-    total_words = len(words)
-    tf = count_target_word / total_words
-    return tf
+
+def term_frequency(target_word: str, target_chapter_idx: int) -> float:
+    data = read_data("class_4/hw3/war_peace.txt")
+    chapter_frequencies = create_chapters_dicts(data)
+
+    if 0 <= target_chapter_idx < len(chapter_frequencies):
+        chapter_data = list(chapter_frequencies[target_chapter_idx].keys())
+        total_words = len(chapter_data)
+        word_count = chapter_frequencies[target_chapter_idx].get(target_word, 0)
+        tf = word_count / total_words if total_words > 0 else 0
+        return tf
+    else:
+        return 0
 
 
-import math
-from collections import Counter
-def get_tf_idf(data, target_word: str, target_chapter: int) -> float:
-    chapter_text = data[target_chapter]
-    words = chapter_text.lower().split()
-    total_words = len(words)
-    term_count = words.count(target_word.lower())
-    if total_words > 0:
-        tf = term_count / total_words
-    else:
-        tf = 0.0
-    total_chapters = len(data)
-    doc_count = sum(1 for chapter in data if target_word.lower() in chapter.lower())
-    if doc_count > 0:
-        idf = math.log(total_chapters / doc_count)
-    else:
-        idf = 0.0
+def get_tf_idf(path: str, target_word: str, target_chapter: int) -> float:
+    data = read_data(path)
+    chapter_frequencies = create_chapters_dicts(data)
+    tf = term_frequency(target_word, target_chapter)
+
+    if tf == 0:
+      return 0
+
+    num_chapters_with_word = 0
+    total_chapters = len(chapter_frequencies)
+    for chapter in chapter_frequencies:
+        if target_word in chapter:
+            num_chapters_with_word += 1
+
+    idf = 0
+    if num_chapters_with_word > 0:
+        idf =  (total_chapters / num_chapters_with_word)
+
     tf_idf = tf * idf
     return tf_idf
+
+
+
+with open("class_4/hw3/class_4/hw3/war_peace.txt", "w") as f:
+  f.write("[new chapter]\n")
+  f.write("the the quick quick brown fox\n")
+  f.write("[new chapter]\n")
+  f.write("the lazy dog\n")
+  f.write("the the\n")
+
+
+print(f"Chapter frequency of 'the': {chapter_frequency('class_4/hw3/war_peace.txt', 'the')}")
+print(f"Term frequency of 'the' in chapter 0: {term_frequency('the', 0)}")
+print(f"TF-IDF of 'the' in chapter 0: {get_tf_idf('class_4/hw3/war_peace.txt', 'the', 0)}")
+print(f"TF-IDF of 'dog' in chapter 1: {get_tf_idf('class_4/hw3/war_peace.txt', 'dog', 1)}")
+
+
+os.remove("class_4/hw3/war_peace.txt")
+
 
